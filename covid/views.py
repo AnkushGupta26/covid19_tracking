9463745a8a8e4ac6263b4c40a680c24aae75e4e0
Your Name <ankushgupta2609@gmail.com>
2021-05-05 11:05:43 +0530

    Total cases update done

--- covid/views.py
+++ covid/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from . import data
 # Create your views here.
 def index(request):
-    return render(request, 'covid/home.html')
+    total = data.total
+    totaltest = data.totaltest
+    return render(request, 'covid/home.html', {'total':total, 'totaltest':totaltest})
